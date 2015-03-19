83dc7cd53e11870bb72772ee1294301a54bfca03
Deven Phillips <deven.phillips@gmail.com>
2015-03-18 20:31:47 -0400

    Working on getting the JS version working

--- src/main/java/com/zanclus/distributed/chat/service/Main.java
+++ src/main/java/com/zanclus/distributed/chat/service/Main.java
@@ -19,20 +19,17 @@ package com.zanclus.distributed.chat.service;
  * limitations under the License.
  * #L%
  */
+import static java.text.SimpleDateFormat.*;
+import static java.time.Instant.now;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.impl.LoggerFactory;
 import io.vertx.ext.apex.Router;
 import io.vertx.ext.apex.handler.StaticHandler;
 import io.vertx.ext.apex.handler.sockjs.BridgeOptions;
 import io.vertx.ext.apex.handler.sockjs.PermittedOptions;
 import io.vertx.ext.apex.handler.sockjs.SockJSHandler;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Date;
 
 /**
@@ -43,36 +40,31 @@ public class Main extends AbstractVerticle {
 
     public static void main(String[] args) {
         Vertx.vertx().deployVerticle(new Main());
+//        Vertx.vertx().deployVerticle("chatverticle.groovy");
+//        Vertx.vertx().deployVerticle("chat.js");
     }
 
     @Override
     public void start() throws Exception {
-        final Logger log = LoggerFactory.getLogger(this.getClass());
         
-        log.debug("Creating router");
         Router router = Router.router(vertx);
         
         final EventBus eb = vertx.eventBus();
         
-        log.debug("Subscribing to 'chat.message.in'");
-        eb.consumer("chat.message.in").handler(message -> {
-            log.debug("Message recieved: "+message.body());
-            String timestamp = SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.MEDIUM).format(Date.from(Instant.now()));
-            eb.publish("chat.message.out", timestamp+": "+message.body());
+        eb.consumer("chat.to.server").handler(message -> {
+            String timestamp = getDateTimeInstance(SHORT, MEDIUM).format(Date.from(now()));
+            eb.publish("chat.to.client", timestamp+": "+message.body());
         });
         
-        log.debug("Creating JS/EventBus bridge options");
         BridgeOptions opts = new BridgeOptions()
-                                    .addInboundPermitted(new PermittedOptions().setAddress("chat.message.in"))
-                                    .addOutboundPermitted(new PermittedOptions().setAddress("chat.message.out"));
+                                    .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
+                                    .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
         
-        log.debug("Adding eventbus route");
         SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
         router.route("/eventbus/*").handler(ebHandler);
         
         router.route().handler(StaticHandler.create("webroot/").setIndexPage("chat.html"));
         
-        log.debug("Starting http server.");
         vertx.createHttpServer().requestHandler(router::accept).listen(8000);
     }
 }
